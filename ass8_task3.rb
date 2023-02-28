2e512ea349004fafcc9e7d70bb7881981529a422
Rutu-Butani <125644767+Rutu-Butani@users.noreply.github.com>
2023-02-28 14:13:01 +0530

    Update ass8_task3.rb

--- ass8_task3.rb
+++ ass8_task3.rb
@@ -1,28 +1,24 @@
-# Input: string = "communication"
-# Expected output: "comminacituon"
-
-def isVowel(c):
-    if (c == 'a' or c == 'A' or c == 'e' or
-        c == 'E' or c == 'i' or c == 'I' or
-        c == 'o' or c == 'O' or c == 'u' or c == 'U'):
-        return True
-    end
+def reverse_vowels(s)
+  vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  i = 0
+  j = s.length - 1
+  while i < j
+    if vowels.include?(s[i]) && vowels.include?(s[j])
+      s[i], s[j] = s[j], s[i]
+   i=i+1
+   j=j-1
+    elsif vowels.include?(s[i])
+      j -= 1
+    elsif vowels.include?(s[j])
+      i += 1
     else
-        return False
-    end
-end
-def reverse_vowels(string)
-    
-    i = string.size
-    j = string.size - 1
-    while i < j
-      if !isVoewl
       i += 1
       j -= 1
-    end
-    return string
+
+   end
   end
-  
-  string = "communication"
-  puts reverse_vowels(string) # Output: "comminacituon"
-  
+  puts "After vowel reverese : #{s}"
+end 
+
+puts "Original string : communication"
+reverse_vowels("communication")
